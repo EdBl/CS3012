f85f669d9c8fa780f81b96aca62ad200bb4eeed5
Edvardas <blazevie@tcd.ie>
2017-10-14 21:29:16 +0100

    Added testing

--- Task1/src/ancestors/Main.java
+++
@@ -1,9 +0,0 @@
-package ancestors;
-
-public class Main {
-
-	public static void main(String[] args){
-		System.out.println("Welcome, world!");
-	}
-	
-}
--- Task1/src/ancestors/Node.java
+++ Task1/src/ancestors/Node.java
@@ -2,4 +2,20 @@ package ancestors;
 
 public class Node {
 
+	public int value;
+	public Node left, right, parent;
+	
+	public Node(Node parent, int value){
+		this.parent = parent;
+		this.value = value;
+	}
+	
+	public void addLeft(int value){
+		left = new Node(this, value);
+	}
+	
+	public void addRight(int value){
+		right = new Node(this, value);
+	}
+	
 }
