cd127412048195d9bc4d0ef5a66c911f3fb6e797
Edvardas <blazevie@tcd.ie>
2017-10-31 12:21:10 +0000

    Implemented getting all parents of a node and added some basic testing for it" git status q :q! Omg Hope you enjoyed watching me fail a commit message

--- Task1/task1/src/Trees.hs
+++ Task1/task1/src/Trees.hs
@@ -1,3 +1,43 @@
-module Trees where
+{-# OPTIONS_GHC -fwarn-incomplete-patterns #-}
 
-wow = "Wow!!"
+module Trees where
+import Data.List (intersect)
+
+data Node = Node String [Node]
+
+instance Eq Node where
+    (Node s _) == (Node t _) = s == t
+
+instance Show Node where
+  show (Node s _) = show s
+
+-------------------------------- Functions related to getting parents below --------
+
+-- Returns a list of all the parents of the node
+getParents :: Node -> [Node]
+getParents (Node s n) = removeDuplicates (getParentsH n)
+
+-- Recursively get a list of all parents of the node
+getParentsH :: [Node] -> [Node]
+getParentsH []     = []
+getParentsH [l]    = [l] ++ (beginParents l)
+getParentsH (l:ls) = [l] ++ (beginParents l) ++ (getParentsH ls)
+
+-- Begins searching the node's parents
+beginParents :: Node -> [Node]
+beginParents (Node _ n) = getParentsH n
+
+
+
+------------------------------------ Misc functions below --------------------------
+
+-- Instead of using show, more concise.
+view :: [Node] -> String
+view [] = []
+view ((Node s _):ls) = s ++ (view ls)
+
+-- Removes all duplicate nodes
+removeDuplicates :: [Node] -> [Node]
+removeDuplicates (x:xs) = x : removeDuplicates (filter (/= x) xs)
+removeDuplicates [] = []
+
--- Task1/task1/test/Spec.hs
+++ Task1/task1/test/Spec.hs
@@ -8,9 +8,20 @@ import Trees
 main :: IO ()
 main = defaultMain tests -- Run the tests
 
+aa, ab :: Trees.Node
+aa = Node "A" []
+ab = Node "B" [aa]
+ac = Node "C" [ab]
+ad = Node "D" [ac]
+ae = Node "E" [ab]
+af = Node "F" [ae]
+ag = Node "G" [ad, af]
+
 tests :: [TF.Test]
-tests = [testGroup "Example of a test\n\n"
-          [ testCase "This should fail" (2+2 @?= 5)
-		  , testCase "This should pass" (2+2 @?= 4)
-		  ]
-		]
+tests = [testGroup "getParents Tests"
+          [ testCase "Get all parents of parentless node" ("" @?= view (getParents aa))
+          , testCase "Get all parents of node with one parent" ("A" @?= view (getParents ab))
+          , testCase "Get all parents of node with a lot of single parents" ("CBA" @?= view (getParents ad))
+          , testCase "Get all parents of node with a lot of varied parents" ("DCBAFE" @?= view (getParents ag))
+          ]
+        ]
