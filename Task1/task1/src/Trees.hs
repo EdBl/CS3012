fb97ae7a2161c7cd055759f34c38e9474da71a38
Edvardas <blazevie@tcd.ie>
2017-10-31 13:58:22 +0000

    Finished implementation and testing

--- Task1/task1/src/Trees.hs
+++ Task1/task1/src/Trees.hs
@@ -11,6 +11,28 @@ instance Eq Node where
 instance Show Node where
   show (Node s _) = show s
 
+
+
+-------------------------------- Functions related to getting LCA below ------------
+
+-- Returns the LCAs of the two nodes
+getLCAs :: Node -> Node -> [Node]
+getLCAs a b = eliminateNonLCAs ( intersect (getParents a) (getParents b))
+
+-- Returns only the LCAs
+eliminateNonLCAs :: [Node] -> [Node]
+eliminateNonLCAs n = removeIfDuplicated (addAllParents n)
+
+-- Go through the list of nodes and add each node's parents to the list
+addAllParents :: [Node] -> [Node]
+addAllParents [] = []
+addAllParents [n] = n : (getDirectParents n)
+addAllParents (n:ns) = n : (getDirectParents n) ++ (addAllParents ns)
+
+  
+  
+  
+  
 -------------------------------- Functions related to getting parents below --------
 
 -- Returns a list of all the parents of the node
@@ -40,4 +62,14 @@ view ((Node s _):ls) = s ++ (view ls)
 removeDuplicates :: [Node] -> [Node]
 removeDuplicates (x:xs) = x : removeDuplicates (filter (/= x) xs)
 removeDuplicates [] = []
+
+-- Removes all occureneces of node if it is duplicated
+removeIfDuplicated :: [Node] -> [Node]
+removeIfDuplicated []     = []
+removeIfDuplicated (x:xs) | duplicated == True  = removeIfDuplicated (filter (/= x) xs)
+                          | duplicated == False = x: removeIfDuplicated xs
+                          where
+						    duplicated = elem x xs
+
+getDirectParents (Node _ n) = n
 
--- Task1/task1/test/Spec.hs
+++ Task1/task1/test/Spec.hs
@@ -2,13 +2,13 @@ import Test.HUnit
 import Test.Framework as TF (defaultMain, testGroup, Test)
 import Test.Framework.Providers.HUnit (testCase)
 import Test.Framework.Providers.QuickCheck2 (testProperty)
+import Data.List (intersect)
 
 import Trees
 
 main :: IO ()
 main = defaultMain tests -- Run the tests
 
-aa, ab :: Trees.Node
 aa = Node "A" []
 ab = Node "B" [aa]
 ac = Node "C" [ab]
@@ -17,11 +17,48 @@ ae = Node "E" [ab]
 af = Node "F" [ae]
 ag = Node "G" [ad, af]
 
+bf = Node "F" []
+be = Node "E" [bf]
+bd = Node "D" [be]
+bc = Node "C" [be]
+bb = Node "B" [bc, bd, be]
+ba = Node "A" [bb]
+bg = Node "G" [bd]
+
+
 tests :: [TF.Test]
-tests = [testGroup "getParents Tests"
+tests = [
+        
+        testGroup "getParents tests for tree A"
           [ testCase "Get all parents of parentless node" ("" @?= view (getParents aa))
           , testCase "Get all parents of node with one parent" ("A" @?= view (getParents ab))
           , testCase "Get all parents of node with a lot of single parents" ("CBA" @?= view (getParents ad))
           , testCase "Get all parents of node with a lot of varied parents" ("DCBAFE" @?= view (getParents ag))
           ]
+          
+        ,testGroup "getParents tests for tree B"
+          [ testCase "Get all parents of parentless node" ("" @?= view (getParents bf))
+          , testCase "Get all parents of node with one parent" ("F" @?= view (getParents be))
+          , testCase "Get all parents of node with a lot of single parents" ("DEF" @?= view (getParents bg))
+          , testCase "Get all parents of node with a lot of varied parents" ("BCEFD" @?= view (getParents ba))
+          ]
+          
+        ,testGroup "getLCAs tests for tree A"
+          [ testCase "Get LCA when theres no LCA" (view (getLCAs aa ag) @?= "")
+          , testCase "Get LCA for the same node" (view (getLCAs ad ad) @?= "C")
+          , testCase "Get LCA for D and F" (view (getLCAs ad af) @?= "B")
+          , testCase "Get LCA for G and B" (view (getLCAs ag ab) @?= "A")
+          , testCase "Get LCA for G and F" (view (getLCAs ag af) @?= "E")
+          ]
+          
+        ,testGroup "getLCAs tests for tree B"
+          [ testCase "Get LCA when theres no LCA" (view (getLCAs ba bf) @?= "")
+          , testCase "Get LCA for the same node" (view (getLCAs ba ba) @?= "B")
+          , testCase "Get LCA for A and G" (view (getLCAs ba bg) @?= "D")
+          , testCase "Get LCA for B and D" (view (getLCAs bb bd) @?= "E")
+          , testCase "Get LCA for B and C" (view (getLCAs bb bc) @?= "E")
+          , testCase "Get LCA for B and E" (view (getLCAs bb be) @?= "F")
+          , testCase "Get LCA for A and D" (view (getLCAs ba bd) @?= "E")
+          ]
+          
         ]
